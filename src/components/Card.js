60d127d7262ec18fe86374ef496fa56f5b2162a4
Chrissy Elwes <96088605+Chrissyelwes@users.noreply.github.com>
2022-10-28 16:08:44 +0100

    Added Card, CardForm and ThankYouCard Components

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <p>this is where the card code goes</p>
+    )
+}
+
+export default Card;
---
+++ src/components/CardForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CardForm = () => {
+    return (
+        <p>this is where the card code goes</p>
+    )
+}
+
+export default CardForm;
